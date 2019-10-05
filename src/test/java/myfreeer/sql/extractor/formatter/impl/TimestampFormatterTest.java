24e923ceccf8c61c4a752f967f798dda27b5a072
myfreeer <myfreeer@users.noreply.github.com>
2019-10-05 11:25:25 +0800

    TimestampFormatter: add unit test
    https://github.com/myfreeer/sql-extractor/issues/1

---
+++ src/test/java/myfreeer/sql/extractor/formatter/impl/TimestampFormatterTest.java
@@ -0,0 +1,46 @@
+package myfreeer.sql.extractor.formatter.impl;
+
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+public class TimestampFormatterTest extends DateFormatterTest {
+  private final TimestampFormatter formatter = new TimestampFormatter(properties);
+
+  @Override
+  protected TimestampFormatter formatter() {
+    return formatter;
+  }
+
+  @Override
+  public void type() {
+    assertEquals(formatter().type(), Types.TIMESTAMP);
+  }
+
+  @Override
+  public void pattern() {
+    assertEquals("yyyy-MM-dd HH:mm:ss.SSSSSS", formatter().pattern());
+  }
+
+  @Override
+  public void conventFn() {
+    assertEquals("TO_TIMESTAMP", formatter().conventFn());
+  }
+
+  @Override
+  public void oraclePattern() {
+    assertEquals("YYYY-MM-DD HH24:MI:SS.FF6", formatter().oraclePattern());
+  }
+
+  @Override
+  public void formatResultSet() throws SQLException {
+    final Date now = new Date();
+    assertEquals(formatter.format(now), formatter.format(resultSet(now), 1));
+  }
+
+  @Override
+  public void nullValue() throws SQLException {
+    assertEquals("null", formatter.format(resultSet(null), 1));
+  }
+}
