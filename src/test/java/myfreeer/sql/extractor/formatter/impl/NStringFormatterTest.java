711a9f36af5e3a47fa81eec642a6fb1d3a6b2395
myfreeer <myfreeer@users.noreply.github.com>
2020-02-23 21:08:39 +0800

    NStringFormatterTest: add override annotation

--- src/test/java/myfreeer/sql/extractor/formatter/impl/NStringFormatterTest.java
+++ src/test/java/myfreeer/sql/extractor/formatter/impl/NStringFormatterTest.java
@@ -11,42 +11,50 @@ public class NStringFormatterTest extends StringFormatterTest {
     private static final NStringFormatter FORMATTER = new NStringFormatter();
 
     @Test
+    @Override
     public void prefix() {
         assertEquals("(nq'[", getFormatter().prefix());
     }
 
     @Test
+    @Override
     public void type() {
         assertEquals(Types.NVARCHAR, getFormatter().type());
     }
 
+    @Override
     protected StringFormatter getFormatter() {
         return FORMATTER;
     }
 
     @Test
+    @Override
     public void escapingDelimiter() {
         assertEquals("]' || n']' || n'[", getFormatter().escapingDelimiter());
     }
 
     @Test
+    @Override
     public void escapingSuffix() {
         assertEquals("]' || n']')", getFormatter().escapingSuffix());
     }
 
     @Test
+    @Override
     public void simpleString() throws SQLException {
         assertEquals("(nq'[" + getRandomStr() + "]')",
                 getFormatter().format(resultSet(getRandomStr()), 1));
     }
 
     @Test
+    @Override
     public void escapingString() throws SQLException {
         assertEquals("(nq'[" + getRandomStr() + "]' || n']' || n'[" + getRandomStr() + "]')",
                 getFormatter().format(resultSet(getRandomStr() + ']' + getRandomStr()), 1));
     }
 
     @Test
+    @Override
     public void escapingStringAtEnd() throws SQLException {
         assertEquals("(nq'[" + getRandomStr() + "]' || n']')",
                 getFormatter().format(resultSet(getRandomStr() + ']'), 1));
